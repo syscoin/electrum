d31a23257aac942c9615e97526759c8fc7f0855c
sidhujag <jsidhu@blockchainfoundry.co>
2019-10-19 13:06:47 -0700

    fix echange rate api

--- electrum/exchange_rate.py
+++ electrum/exchange_rate.py
@@ -22,12 +22,13 @@ from .logging import Logger
 
 
 DEFAULT_ENABLED = True
-DEFAULT_CURRENCY = "BTC"
+DEFAULT_CURRENCY = "USD"
 DEFAULT_EXCHANGE = "CoinGecko"  # default exchange should ideally provide historical rates
 
 
 # See https://en.wikipedia.org/wiki/ISO_4217
-CCY_PRECISIONS = {'BHD': 3, 'BIF': 0, 'BYR': 0, 'CLF': 4, 'CLP': 0,
+CCY_PRECISIONS = {'USD': 4, 'EUR': 4, 'BTC': 8, 'ETH': 8, 'CAD': 4,
+                  'BHD': 3, 'BIF': 0, 'BYR': 0, 'CLF': 4, 'CLP': 0,
                   'CVE': 0, 'DJF': 0, 'GNF': 0, 'IQD': 3, 'ISK': 0,
                   'JOD': 3, 'JPY': 0, 'KMF': 0, 'KRW': 0, 'KWD': 3,
                   'LYD': 3, 'MGA': 1, 'MRO': 1, 'OMR': 3, 'PYG': 0,
@@ -145,6 +146,8 @@ class ExchangeBase(Logger):
         return sorted([str(a) for (a, b) in rates.items() if b is not None and len(a)==3])
 
 
+# Doesn't support Syscoin.
+"""
 class BitcoinAverage(ExchangeBase):
     # note: historical rates used to be freely available
     # but this is no longer the case. see #5188
@@ -154,14 +157,21 @@ class BitcoinAverage(ExchangeBase):
         return dict([(r.replace("BTC", ""), Decimal(json[r]['last']))
                      for r in json if r != 'timestamp'])
 
+"""
 
+
+# Doesn't support Syscoin.
+"""
 class Bitcointoyou(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('bitcointoyou.com', "/API/ticker.aspx")
         return {'BRL': Decimal(json['ticker']['last'])}
 
+"""
 
+# Doesn't support Syscoin.
+"""
 class BitcoinVenezuela(ExchangeBase):
 
     async def get_rates(self, ccy):
@@ -177,36 +187,51 @@ class BitcoinVenezuela(ExchangeBase):
         json = await self.get_json('api.bitcoinvenezuela.com',
                              "/historical/index.php?coin=BTC")
         return json[ccy +'_BTC']
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class Bitbank(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('public.bitbank.cc', '/btc_jpy/ticker')
         return {'JPY': Decimal(json['data']['last'])}
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class BitFlyer(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('bitflyer.jp', '/api/echo/price')
         return {'JPY': Decimal(json['mid'])}
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class BitPay(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('bitpay.com', '/api/rates')
         return dict([(r['code'], Decimal(r['rate'])) for r in json])
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class Bitso(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('api.bitso.com', '/v2/ticker')
         return {'MXN': Decimal(json['last'])}
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class BitStamp(ExchangeBase):
 
     async def get_currencies(self):
@@ -217,42 +242,61 @@ class BitStamp(ExchangeBase):
             json = await self.get_json('www.bitstamp.net', f'/api/v2/ticker/btc{ccy.lower()}/')
             return {ccy: Decimal(json['last'])}
         return {}
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class Bitvalor(ExchangeBase):
 
     async def get_rates(self,ccy):
         json = await self.get_json('api.bitvalor.com', '/v1/ticker.json')
         return {'BRL': Decimal(json['ticker_1h']['total']['last'])}
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class BlockchainInfo(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('blockchain.info', '/ticker')
         return dict([(r, Decimal(json[r]['15m'])) for r in json])
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class Bylls(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('bylls.com', '/api/price?from_currency=BTC&to_currency=CAD')
         return {'CAD': Decimal(json['public_price']['to_price'])}
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class Coinbase(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('api.coinbase.com',
                              '/v2/exchange-rates?currency=BTC')
         return {ccy: Decimal(rate) for (ccy, rate) in json["data"]["rates"].items()}
+"""
 
 
 class CoinCap(ExchangeBase):
 
     async def get_rates(self, ccy):
+        # CoinCap rates API is broken for Syscoin.  Use history API instead.
+        """
         json = await self.get_json('api.coincap.io', '/v2/rates/bitcoin/')
         return {'USD': Decimal(json['data']['rateUsd'])}
+        """
+        json = await self.get_json('api.coincap.io',
+                                      '/v2/assets/syscoin/history?interval=d1&limit=2000')
+        return {'USD': Decimal(json['data'][-1]['priceUsd'])}
 
     def history_ccys(self):
         return ['USD']
@@ -261,11 +305,13 @@ class CoinCap(ExchangeBase):
         # Currently 2000 days is the maximum in 1 API call
         # (and history starts on 2017-03-23)
         history = await self.get_json('api.coincap.io',
-                                      '/v2/assets/bitcoin/history?interval=d1&limit=2000')
+                                      '/v2/assets/syscoin/history?interval=d1&limit=2000')
         return dict([(datetime.utcfromtimestamp(h['time']/1000).strftime('%Y-%m-%d'), h['priceUsd'])
                      for h in history['data']])
 
 
+# Doesn't support Syscoin.
+"""
 class CoinDesk(ExchangeBase):
 
     async def get_currencies(self):
@@ -293,14 +339,21 @@ class CoinDesk(ExchangeBase):
                  % (start, end))
         json = await self.get_json('api.coindesk.com', query)
         return json['bpi']
+"""
 
 
 class CoinGecko(ExchangeBase):
 
     async def get_rates(self, ccy):
+        # CoinGecko exchange_rates API is unavailable for Syscoin.  Use coins API instead.
+        """
         json = await self.get_json('api.coingecko.com', '/api/v3/exchange_rates')
         return dict([(ccy.upper(), Decimal(d['value']))
                      for ccy, d in json['rates'].items()])
+        """
+        json = await self.get_json('api.coingecko.com', '/api/v3/coins/syscoin')
+        return dict([(ccy.upper(), Decimal(d))
+                     for ccy, d in json['market_data']['current_price'].items()])
 
     def history_ccys(self):
         # CoinGecko seems to have historical data for all ccys it supports
@@ -314,6 +367,7 @@ class CoinGecko(ExchangeBase):
                      for h in history['prices']])
 
 
+"""
 class itBit(ExchangeBase):
 
     async def get_rates(self, ccy):
@@ -323,8 +377,11 @@ class itBit(ExchangeBase):
         if ccy in ccys:
             result[ccy] = Decimal(json['lastPrice'])
         return result
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class Kraken(ExchangeBase):
 
     async def get_rates(self, ccy):
@@ -334,37 +391,52 @@ class Kraken(ExchangeBase):
                              '/0/public/Ticker?pair=%s' % ','.join(pairs))
         return dict((k[-3:], Decimal(float(v['c'][0])))
                      for k, v in json['result'].items())
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class LocalBitcoins(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('localbitcoins.com',
                              '/bitcoinaverage/ticker-all-currencies/')
         return dict([(r, Decimal(json[r]['rates']['last'])) for r in json])
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class MercadoBitcoin(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('api.bitvalor.com', '/v1/ticker.json')
         return {'BRL': Decimal(json['ticker_1h']['exchanges']['MBT']['last'])}
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class NegocieCoins(ExchangeBase):
 
     async def get_rates(self,ccy):
         json = await self.get_json('api.bitvalor.com', '/v1/ticker.json')
         return {'BRL': Decimal(json['ticker_1h']['exchanges']['NEG']['last'])}
+"""
 
+# Doesn't support Syscoin.
+"""
 class TheRockTrading(ExchangeBase):
 
     async def get_rates(self, ccy):
         json = await self.get_json('api.therocktrading.com',
                              '/v1/funds/BTCEUR/ticker')
         return {'EUR': Decimal(json['last'])}
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class Winkdex(ExchangeBase):
 
     async def get_rates(self, ccy):
@@ -380,12 +452,16 @@ class Winkdex(ExchangeBase):
         history = json['series'][0]['results']
         return dict([(h['timestamp'][:10], h['price'] / 100.0)
                      for h in history])
+"""
 
 
+# Doesn't support Syscoin.
+"""
 class Zaif(ExchangeBase):
     async def get_rates(self, ccy):
         json = await self.get_json('api.zaif.jp', '/api/1/last_price/btc_jpy')
         return {'JPY': Decimal(json['last_price'])}
+"""
 
 
 def dictinvert(d):
